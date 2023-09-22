4798dccd2dfaa74b7a611ab21d7bc49d8a723ebd
Mark McPherson <mark@mcpherson.me.uk>
2023-09-22 14:58:48 +0100

    Add comment

--- infrastructure/lib/consts.ts
+++ infrastructure/lib/consts.ts
@@ -4,11 +4,11 @@ export class Constants {
     public static readonly GitHubRepo = 'coding-challenge';  // Name of the source repo for our app in github
     public static readonly GitHubOwner = 'markmcpherson';  // Owner of the source repo for our app in github
     public static readonly GitHubBranch = 'main'; // Branch to build/deploy
-    public static readonly GitHubTokenSecretName = 'dev/github-access-token'; // Name of the secret in AWS SecretsManager containing the github oauth token
+    public static readonly GitHubTokenSecretName = 'dev/github-access-token'; // Name of the secret in AWS SecretsManager containing the github oauth token (assumed to already exist)
 
     // overridable constants 
-    public static readonly EKSClusterName = process.env.EKS_CLUSTER_NAME || ' eks-dev-cluster';
+    public static readonly EKSClusterName = process.env.EKS_CLUSTER_NAME || ' eks-dev-cluster'; // eks cluster name. This is assumed to already exist
     public static readonly DeploymentEnvironment = process.env.DEPLOY_ENVIRONMENT || 'dev'; // e.g. dev/staging/prod. There is helm chart values file for dev.
-    public static readonly DeploymentNamespace = process.env.DEPLOY_NAMESPACE || 'dev';
+    public static readonly DeploymentNamespace = process.env.DEPLOY_NAMESPACE || 'dev'; // eks namespace. This is assumed to already exist
     public static readonly ApplicationName = process.env.APPLICATION_NAME || 'application';
 }
